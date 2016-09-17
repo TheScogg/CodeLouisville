6a6a13d385823665fb4dc2fc0dd403fa6428ea67
Chris Scoggins <thescogg@gmail.com>
2016-09-16 21:26:08 -0400

    Added skeleton files for project

---
+++ aDataRemember/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Chris on 9/16/2016.
+ */
+console.log("JS Running");
+
+$(document).ready(function () {
+    //Draw buttons for daily activities
+    var drawButtons = function () {
+
+    }
+
+});
